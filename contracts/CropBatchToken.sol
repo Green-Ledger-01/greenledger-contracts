ee225ed69061afe82a19c18b7b7eb4f8c6fece5e
oumaoumag <ouma.godwin10@gmail.com>
2025-06-05 23:17:51 +0300

    feat: update CropBatchToken imports and add ReentrancyGuard

--- contracts/CropBatchToken.sol
+++ contracts/CropBatchToken.sol
@@ -1,4 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
-import "openzappelib/contracts/token/ERC1155/ERC155.sol";
+import "@thirdweb-dev/contracts/base/ERC1155Base.sol";
+import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+// TODO:
+Implement Contract Logic
