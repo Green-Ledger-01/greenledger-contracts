23077c4483b565e6e6f329c035945e6bc2761546
oumaoumag <ouma.godwin10@gmail.com>
2025-06-05 23:28:19 +0300

    feat: implement initial CropBatchToken contract structure

--- contracts/CropBatchToken.sol
+++ contracts/CropBatchToken.sol
@@ -5,5 +5,11 @@ import "@thirdweb-dev/contracts/base/ERC1155Base.sol";
 import "@thirdweb-dev/contracts/extension/PermissionsEnumerable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-// TODO:
-Implement Contract Logic
+/**
+ * @title CropBatchToken
+ * @dev ERC1155 contract for tokenizing for tokenizing unique crop batches as NFTs for GreenLedger.
+ * Each token ID represents a unique batch with a supply of 1.
+ */ 
+contract CropBatchToken is ERC1155Base, PermissionsEnumerable, ReentrancyGuard {
+    
+}
